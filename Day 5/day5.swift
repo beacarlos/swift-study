ac345f3a9533cb3bf8e5b2bf5da1db8b1527df59
Beatriz Carlos <beatrizacarlos2000@gmail.com>
2020-03-23 12:32:20 -0300

    Atualizaçao do day 5

--- Day 5/day5.swift
+++ Day 5/day5.swift
@@ -9,15 +9,15 @@ helloWord()
 // Function - Accepting Parameters
 //Data com mês por extenso. Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
 
-// var data_separada: [String] = []
-// var data: String = "19/06/2000"
+var data_separada: [String] = []
+var data: String = "19/06/2000"
 
-// func dataExtensao(data: String) -> [String] {
-//     return data.components(separatedBy: ["-", "*", "/"])
-// }
+func dataExtensao(data: String) -> [String] {
+    return data.components(separatedBy: ["-", "*", "/"])
+}
 
-// data_separada = dataExtensao(data: data)
-// print(data_separada)
+data_separada = dataExtensao(data: data)
+print(data_separada)
 
 // Parameter labels
 // É um apelido.
